474b6849d5d5820a88899801e87b0217d7056568
Trinh Tan Tai <fxqtai@gmail.com>
2020-10-27 23:01:24 +0700

    HELLO

--- configs/database.js
+++ configs/database.js
@@ -1,9 +1,12 @@
 const Sequelize = require('sequelize');
 
-module.exports = new Sequelize('sprintretroapp', 'postgres', 'root', {
-    host: 'localhost',
+module.exports = new Sequelize('db7hvqs99lfjf7', 'zhzpczjxjusovy', 'd3f71536a301a6098c28cc681a05bd1f23072c7e8569dbb08048d41ce3827729', {
+    host: 'ec2-54-82-208-124.compute-1.amazonaws.com',
     dialect: 'postgres',
     ssl: true,
+    dialectOptions: {
+        "ssl": { "require": true, rejectUnauthorized: false }
+    },
     pool: {
         max: 15,
         min: 0,
